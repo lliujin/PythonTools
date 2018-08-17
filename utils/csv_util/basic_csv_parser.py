de1b857c30dca9a2af34867797e5b97361f03c56
zonghuixu <zonghui.xu@coupang.com>
2018-08-17 12:42:37 +0800

    prepare to github

---
+++ utils/csv_util/basic_csv_parser.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+__author__ = 'zonghuixu'
+
+import pandas
+from os import listdir
+from os.path import isfile, join
+
+BASE_DIRS = ['/Users/zonghuixu/Desktop', '/Users/zonghuixu/Downloads', '/Users/zonghuixu/Documents']
+
+
+def get_data_frame(filename, specific_path=None):
+    if specific_path:
+        file_path = join(specific_path, filename)
+    else:
+        target_files = []
+        for directory in BASE_DIRS:
+            files = [join(directory, f) for f in listdir(directory) if isfile(join(directory, f)) and f == filename]
+            target_files += files
+        length = len(target_files)
+
+        if length > 1:
+            raise Exception("too many files found!", filename)
+        elif length == 0:
+            raise Exception("0 file found!", filename)
+        else:
+            file_path = target_files[0]
+    return pandas.read_csv(file_path)
