75ec7a323f53540d7bced97c09a733c750e26b14
Liu_Jin <389594458@qq.com>
2018-08-21 22:05:37 +0800

    mapandreduce

--- test/python_map_reduce.py
+++ test/python_map_reduce.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
 
-# practice capitalize with map()
+# practice： capitalize with map()
 def normalize(l1):
     return l1[0].upper() + l1[1:].lower()
 
@@ -10,4 +10,30 @@ l1 = ['dff', 'HkEEf', 'dfjOFF']
 print(list(map(normalize, l1)))
 
 
-# practice product with reduce
+# practice: product with reduce（）
+def fn(x, y):
+    return x * y
+
+
+def prod(l2):
+    return reduce(fn, l2)
+
+
+print(prod([3, 5, 7, 9]))
+
+
+# practice: str to float with map() and reduce()
+def str_to_num(s):
+    def f(x1, y1):
+        return x1 * 10 + y1
+    s = s.split('.')
+    if len(s) == 1:
+        return reduce(f, map(int, s[0]))
+    else:
+        return reduce(f, map(int, s[0] + s[1])) / pow(10, len(s[1]))
+
+
+print(str_to_num('123.456'))
+print(str_to_num('0.123'))
+print(str_to_num('123.'))
+print(str_to_num('123'))
