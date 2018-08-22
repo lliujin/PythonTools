04f22c8fe6b3f6bd796c4202ec8031e96e1e1018
Liu_Jin <389594458@qq.com>
2018-08-22 20:03:53 +0800

    practice sorted()

---
+++ test/python_sorted.py
@@ -0,0 +1,14 @@
+# practice： sorted()
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+
+def by_name(t):
+    return t[0]
+
+
+def by_score(t):
+    return t[1]
+
+
+print(sorted(L, key=by_name))
+print(sorted(L, key=by_score, reverse=True))
