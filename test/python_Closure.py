a05532b30f9d951a6101a5e6a112833838b8d90b
Liu_Jin <389594458@qq.com>
2018-08-24 16:02:55 +0800

    practice： Closure

--- test/python_Closure.py
+++ test/python_Closure.py
@@ -1,16 +1,33 @@
-@metric
-def fast(x, y):
-    time.sleep(0.0012)
-    return x + y;
+# practice: Closure
+def counter():
+    L = [0]
 
-@metric
-def slow(x, y, z):
-    time.sleep(0.1234)
-    return x * y * z;
+    def count():
+        L[0] = L[0] + 1
+        return L[0]
+    return count
 
-f = fast(11, 22)
-s = slow(11, 22, 33)
-if f != 33:
+
+counterA = counter()
+print(counterA(), counterA(), counterA(), counterA(), counterA())     # 1 2 3 4 5
+counterB = counter()
+if [counterB(), counterB(), counterB(), counterB()] == [1, 2, 3, 4]:
+    print('测试通过!')
+else:
     print('测试失败!')
-elif s != 7986:
-    print('测试失败!')
+
+
+# Closure does mot reference any loop variables
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
+    return fs
+
+
+f1, f2, f3 = count()
+print(f1())
+print(f1())
+print(f1())
