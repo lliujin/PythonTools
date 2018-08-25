a19278e6422b98fe2863135084825676243fa031
Liu_Jin <389594458@qq.com>
2018-08-25 09:17:34 +0800

    practice： Closure(add an example)

--- test/python_Closure.py
+++ test/python_Closure.py
@@ -23,11 +23,31 @@ def count():
     for i in range(1, 4):
         def f():
             return i*i
-        fs.append(f)
+        a = f
+        fs.append(a)
     return fs
 
 
 f1, f2, f3 = count()
 print(f1())
+print(f2())
+print(f3())
+
+
+# compare with the last example
+def count():
+    def f(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i))    # f(i)立刻被执行，因此i的当前值被传入f()
+    return fs
+
+
+f1, f2, f3 = count()
 print(f1())
-print(f1())
+print(f2())
+print(f3())
+
